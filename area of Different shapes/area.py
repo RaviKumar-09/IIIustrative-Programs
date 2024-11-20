e449bda422cef2142a3fd06b6c05f32b148567c0
RaviKumar <ravikumarainapudi97@gmail.com>
2024-11-20 19:56:07 +0530

    IndexError: list index out of range

--- area of Different shapes/area.py
+++ area of Different shapes/area.py
@@ -5,6 +5,6 @@ areaCircle = pi*radius**2
 print(areaCircle)
 area = input('Enter the length and breath of a rectangle separated by spaces:')
 length = float(area.split()[0])
-breath = float(area.split()[1])
+breath = float(area.split()[0])
 parameter = length*breath
 print(parameter)
