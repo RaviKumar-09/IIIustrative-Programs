65bdaecc301b80806b5f36dbe4a26409dc196eaa
RaviKumar <ravikumarainapudi97@gmail.com>
2024-11-20 19:59:23 +0530

    #area of the circle

--- area of Different shapes/area.py
+++ area of Different shapes/area.py
@@ -2,6 +2,7 @@ radius = float(input("Enter the radius"))
 #importing module
 import math
 pi = math.pi
+#area of the circle
 areaCircle = pi*radius**2
 print(areaCircle)
 area = input('Enter the length and breath of a rectangle separated by spaces:')
