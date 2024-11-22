137deaa32db0b8452bac93ff3fa2fdfaa85f00d2
RaviKumar <ravikumarainapudi97@gmail.com>
2024-11-22 22:00:36 +0530

    given number is prime

--- given number is prime/prime.py
+++ given number is prime/prime.py
@@ -3,4 +3,12 @@ import math
 prime = True
 x = int(math.sqrt(num))
 while x>1:
-    
+    if num%x == 0:
+        print(x,'divides',num)
+        prime = False
+        break
+    else:
+        x -=1
+    if prime:
+        print('The number',num,'is prime')
+        
