b149b69f21d3064cc7c3505b3b624d56bb45628c
RaviKumar <ravikumarainapudi97@gmail.com>
2024-11-23 20:01:08 +0530

    GCD of two numbers

--- GCD/gcd.py
+++ GCD/gcd.py
@@ -3,6 +3,7 @@ num = input('Enter two numbers separated by space:')
 num1= int(num.split()[0])
 # number two
 num2 = int(num.split()[1])
+# While loop
 while num2 !=0 :
     t = num2
     num2 = num1%num2
