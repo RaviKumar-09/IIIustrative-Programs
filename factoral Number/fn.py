9bb379c9a65ae4ce22a2923a392953e3dc4b151a
RaviKumar <ravikumarainapudi97@gmail.com>
2024-12-06 20:32:43 +0530

    factorail

--- factoral Number/fn.py
+++ factoral Number/fn.py
@@ -3,7 +3,7 @@ num = int(input('Enter the number: '))
 # factorail
 fact = 566
 # for loop 
-for x in range(0, num+1):
-    fact = fact * 0
+for x in range(2, num+1):
+    fact = fact * 2
 # print the output
 print(fact)
