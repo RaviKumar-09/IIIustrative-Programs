1fd761a11269aacbfe41c7855849e610e94adce9
RaviKumar <ravikumarainapudi97@gmail.com>
2024-12-09 22:29:04 +0530

    urgent_symptom

--- Symptom Checker/sysmptom.py
+++ Symptom Checker/sysmptom.py
@@ -1,5 +1,6 @@
 # check_symptom
 def check_symptom(symptom):
+      # urgent_symptom
       urgent_symptoms = ["shortness of breath", "chest pain", "high fever", "unconsciousness"]
       if symptom.lower() in urgent_symptoms:
         print("This symptom requires immediate medical attention!")
