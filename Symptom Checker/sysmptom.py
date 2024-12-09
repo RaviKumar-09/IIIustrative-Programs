be31417ccf7420941758502e8981d88932b3cb4d
RaviKumar <ravikumarainapudi97@gmail.com>
2024-12-09 22:27:04 +0530

    remove else part

--- Symptom Checker/sysmptom.py
+++ Symptom Checker/sysmptom.py
@@ -2,7 +2,7 @@ def check_symptom(symptom):
       urgent_symptoms = ["shortness of breath", "chest pain", "high fever", "unconsciousness"]
       if symptom.lower() in urgent_symptoms:
         print("This symptom requires immediate medical attention!")
-    else:
+   # else:
         print("Monitor your symptom. If it worsens, consult a doctor.")
 # Example usage
 symptom = input("Enter a symptom: ")
