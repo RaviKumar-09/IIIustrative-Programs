925d38e81e72a52f98c48a03346a259cf7940575
RaviKumar <ravikumarainapudi97@gmail.com>
2024-12-09 22:28:45 +0530

    check_symptom

--- Symptom Checker/sysmptom.py
+++ Symptom Checker/sysmptom.py
@@ -1,3 +1,4 @@
+# check_symptom
 def check_symptom(symptom):
       urgent_symptoms = ["shortness of breath", "chest pain", "high fever", "unconsciousness"]
       if symptom.lower() in urgent_symptoms:
