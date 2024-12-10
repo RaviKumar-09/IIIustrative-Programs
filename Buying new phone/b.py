47c65b76082d0470d0c0c4141f6965405ca45a45
RaviKumar <ravikumarainapudi97@gmail.com>
2024-12-10 21:53:10 +0530

    A simple Python program to suggest a phone based on your preferences

---
+++ Buying new phone/b.py
@@ -0,0 +1,9 @@
+# A simple Python program to suggest a phone based on your preferences
+
+phones = [
+    {"name": "iPhone 15", "price": 1200, "camera": 48, "battery": 5000, "storage": 128},
+    {"name": "Samsung Galaxy S23", "price": 1000, "camera": 50, "battery": 4500, "storage": 256},
+    {"name": "Google Pixel 8", "price": 800, "camera": 48, "battery": 4300, "storage": 128},
+    {"name": "OnePlus 11", "price": 700, "camera": 50, "battery": 5000, "storage": 256},
+    {"name": "Xiaomi 13", "price": 600, "camera": 54, "battery": 4700, "storage": 128},
+]
