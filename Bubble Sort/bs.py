c29acbc25e2abd2ca980269dcd02c9b7b0e1c5dd
RaviKumar <ravikumarainapudi97@gmail.com>
2024-12-20 18:32:16 +0530

    for loop

--- Bubble Sort/bs.py
+++ Bubble Sort/bs.py
@@ -3,6 +3,8 @@
 numbers = [64, 34, 25, 12, 22, 11, 90]
 
 # Bubble sort algorithm
+
+# for loop 
 for i in range(len(numbers)):
     for j in range(0, len(numbers) - i - 1):
         if numbers[j] > numbers[j + 1]:
