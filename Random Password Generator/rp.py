6623bcd2681d528984235723082a44549b64791b
RaviKumar <ravikumarainapudi97@gmail.com>
2024-12-25 23:12:44 +0530

    import

--- Random Password Generator/rp.py
+++ Random Password Generator/rp.py
@@ -1,6 +1,10 @@
+# import
 import random
 import string
 
 
 # Generate a random password of length 8
 password = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+
+
+print("Generated password:", password)
