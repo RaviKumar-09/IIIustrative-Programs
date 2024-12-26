3ed0849a970091c6f1fe0e65f88ad89f9af65239
RaviKumar <ravikumarainapudi97@gmail.com>
2024-12-26 17:24:57 +0530

    print

--- Rock-Paper-Scissors Game/rp.py
+++ Rock-Paper-Scissors Game/rp.py
@@ -11,3 +11,4 @@ if player1 == player2:
 elif (player1 == "rock" and player2 == "scissors") or \
      (player1 == "scissors" and player2 == "paper") or \
     (player1 == "paper" and player2 == "rock"):
+    print("Player 1 wins!")
