fc2359471ba76620ff7676cead31eca8300e425f
RaviKumar <ravikumarainapudi97@gmail.com>
2024-12-28 16:10:15 +0530

    is not an Armstrong number

--- Check Armstrong Number/Armstrong.py
+++ Check Armstrong Number/Armstrong.py
@@ -7,3 +7,4 @@ sum_of_powers = sum(int(digit) ** len(str(num)) for digit in str(num))
 if num == sum_of_powers:
     print(f"{num} is an Armstrong number.")
 else:
+    print(f"{num} is not an Armstrong number.")
