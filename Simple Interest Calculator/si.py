9c5c0f0852fc576197d742ce3b30c18f78e9337e
RaviKumar <ravikumarainapudi97@gmail.com>
2024-12-30 23:08:08 +0530

    Calculate simple interest

--- Simple Interest Calculator/si.py
+++ Simple Interest Calculator/si.py
@@ -2,3 +2,5 @@
 principal = float(input("Enter the principal amount: "))
 rate = float(input("Enter the interest rate: "))
 time = float(input("Enter the time in years: "))
+
+# Calculate simple interest
