bf0f0719022e66f13ccc791d7024caa837299a77
RaviKumar <ravikumarainapudi97@gmail.com>
2024-12-30 23:07:49 +0530

    time

--- Simple Interest Calculator/si.py
+++ Simple Interest Calculator/si.py
@@ -1,3 +1,4 @@
 # Input principal, rate, and time
 principal = float(input("Enter the principal amount: "))
 rate = float(input("Enter the interest rate: "))
+time = float(input("Enter the time in years: "))
