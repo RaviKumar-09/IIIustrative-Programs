ca80d450f250b53aeb637c797edc4d9a0d33c5a1
RaviKumar <ravikumarainapudi97@gmail.com>
2024-12-31 23:13:47 +0530

    frequency

--- Count Frequency of Each Character/cf.py
+++ Count Frequency of Each Character/cf.py
@@ -1,3 +1,9 @@
 # Input a string
 text = input("Enter a string: ")
 # Count frequency
+frequency = {}
+for char in text:
+    if char in frequency:
+        frequency[char] += 1
+    else:
+        frequency[char] = 1
