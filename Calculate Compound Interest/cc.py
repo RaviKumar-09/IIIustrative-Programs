8562e2bc532cf673e4392737afe337733e93b91e
RaviKumar <ravikumarainapudi97@gmail.com>
2025-01-04 22:51:54 +0530

    print statement

--- Calculate Compound Interest/cc.py
+++ Calculate Compound Interest/cc.py
@@ -6,4 +6,6 @@ time = int(input("Enter the time in years: "))
 # Calculate compound interest
 amount = principal * (1 + rate / 100) ** time
 compound_interest = amount - principal
+
+# print statement
 print(f"Compound Interest: {compound_interest}")
