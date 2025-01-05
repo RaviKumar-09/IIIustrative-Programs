d9ed0e8f6a05756d9630e94adf5c853f5c265944
RaviKumar <ravikumarainapudi97@gmail.com>
2025-01-05 23:02:55 +0530

    if condition

--- Perfect Number/pn.py
+++ Perfect Number/pn.py
@@ -6,6 +6,7 @@ num = int(input("Enter a number: "))
 # Check if perfect number
 divisors_sum = sum(i for i in range(1, num) if num % i == 0)
 
+# if condition
 if divisors_sum == num:
      print(f"{num} is a perfect number.")
 else:
