1f31104afe463eac07605fc8f54be20914e679cc
RaviKumar <ravikumarainapudi97@gmail.com>
2025-01-15 20:39:54 +0530

    print

--- Check if Two Strings are Anagrams/st.py
+++ Check if Two Strings are Anagrams/st.py
@@ -7,3 +7,4 @@ str2 = input("Enter the second string: ")
 if sorted(str1.lower()) == sorted(str2.lower()):
     print(f"{str1} and {str2} are anagrams.")
 else:
+    print(f"{str1} and {str2} are not anagrams.")
