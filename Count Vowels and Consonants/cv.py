d15e3d245906952ff51961daa13331b2ada4979e
RaviKumar <ravikumarainapudi97@gmail.com>
2025-01-16 22:01:58 +0530

    consonant_count

--- Count Vowels and Consonants/cv.py
+++ Count Vowels and Consonants/cv.py
@@ -4,3 +4,4 @@ text = input("Enter a string: ")
 # Count vowels and consonants
 vowels = "aeiou"
 vowel_count = sum(1 for char in text.lower() if char in vowels)
+consonant_count = sum(1 for char in text.lower() if char.isalpha() and char not in vowels)
