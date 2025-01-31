f942a2f8ef05abeb383997d90e28fb4d3e511d53
RaviKumar <ravikumarainapudi97@gmail.com>
2025-01-31 22:41:59 +0530

    Function to mark attendance

--- Attendance Management/am.py
+++ Attendance Management/am.py
@@ -3,3 +3,8 @@ attendance = {}
 
 # List of students (you can add more students as needed)
 students = ["Alice", "Bob", "Charlie", "David", "Eva"]
+
+#Function to mark attendance
+def mark_attendance():
+    print("\nMarking Attendance:")
+    for student in students:
