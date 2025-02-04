173fc1a728289b04bff9f450641b6bfe5deeaccc
RaviKumar <ravikumarainapudi97@gmail.com>
2025-02-04 23:06:01 +0530

    print

--- Class Eligibility/ce.py
+++ Class Eligibility/ce.py
@@ -2,3 +2,5 @@
 
 def can_attend_class(total_classes, attended_classes, min_attendance=75):
     attendance_percentage = (attended_classes / total_classes) * 100
+
+    print(f"Attendance Percentage: {attendance_percentage:.2f}%")
