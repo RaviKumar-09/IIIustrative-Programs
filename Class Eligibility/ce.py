ae2ccc576bdc8230fd8644676105570e61d5bb26
RaviKumar <ravikumarainapudi97@gmail.com>
2025-02-04 23:05:34 +0530

    can_attend_class

--- Class Eligibility/ce.py
+++ Class Eligibility/ce.py
@@ -1,1 +1,4 @@
 # Function to check if the student can attend class
+
+def can_attend_class(total_classes, attended_classes, min_attendance=75):
+    attendance_percentage = (attended_classes / total_classes) * 100
