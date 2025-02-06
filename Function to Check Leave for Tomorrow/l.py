dfaf6323a304003ba847fafd3a87041039784956
RaviKumar <ravikumarainapudi97@gmail.com>
2025-02-06 23:20:36 +0530

    print

--- Function to Check Leave for Tomorrow/l.py
+++ Function to Check Leave for Tomorrow/l.py
@@ -3,5 +3,6 @@ from datetime import datetime, timedelta
 
 # Check if tomorrow is a weekend
 if (datetime.now() + timedelta(days=1)).weekday() >= 5:
-     print("You can take leave tomorrow. 😊")
+    print("You can take leave tomorrow. 😊")
 else:
+    print("It's a working day. You cannot take leave. 😞")
