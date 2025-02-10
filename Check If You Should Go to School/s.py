2c6c4227410b6bfcf024d3f6dabae76a8ef72899
RaviKumar <ravikumarainapudi97@gmail.com>
2025-02-10 23:43:20 +0530

    print

--- Check If You Should Go to School/s.py
+++ Check If You Should Go to School/s.py
@@ -10,3 +10,4 @@ day = now.strftime("%A")  # Get current day (e.g., Monday, Tuesday)
 # Check if it's a school day and morning time
 
 if day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"] and hour < 8:
+    print("Good to go to school! 📚🏫")
