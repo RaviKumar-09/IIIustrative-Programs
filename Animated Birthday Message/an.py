efa24d50313e9d73c0f9fa44bf13857c82b2c3d6
RaviKumar <ravikumarainapudi97@gmail.com>
2025-02-14 23:23:20 +0530

    print

--- Animated Birthday Message/an.py
+++ Animated Birthday Message/an.py
@@ -5,3 +5,4 @@ message = "🎉🎂 Happy Birthday to You! 🎂🎉"
 for char in message:
     print(char, end="", flush=True)
     time.sleep(0.1)  # Delay to create animation effect
+print("\nHave a fantastic day! 😊")
