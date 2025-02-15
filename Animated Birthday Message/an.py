4535b08bc41fb6e3516dee8663bfc23ee3833bdf
RaviKumar <ravikumarainapudi97@gmail.com>
2025-02-15 23:50:48 +0530

    for loop

--- Animated Birthday Message/an.py
+++ Animated Birthday Message/an.py
@@ -4,6 +4,8 @@ import time
 # message
 message = "🎉🎂 Happy Birthday to You! 🎂🎉"
 
+
+# for loop
 for char in message:
     print(char, end="", flush=True)
     time.sleep(0.1)  # Delay to create animation effect
