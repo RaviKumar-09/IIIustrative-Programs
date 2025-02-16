72bbfafc0b68032663b036f4607551b40a287b9c
RaviKumar <ravikumarainapudi97@gmail.com>
2025-02-16 22:59:37 +0530

    Decision making

--- Slipper Buying Decision python Copy Edit/ss.py
+++ Slipper Buying Decision python Copy Edit/ss.py
@@ -1,2 +1,8 @@
 # Current slipper condition
 slipper_condition = input("Enter slipper condition (Good/Worn Out): ").strip().lower()
+
+# Decision making
+if slipper_condition == "worn out":
+    print("Your slippers are worn out! 🥿➡️🛍️ Time to buy new ones!")
+else:
+    print("Your slippers are still in good condition. No need to buy new ones! 😊")
