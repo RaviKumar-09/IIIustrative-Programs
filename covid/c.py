09cea1691132f29e535d41e9ab105135a64f54bb
RaviKumar <ravikumarainapudi97@gmail.com>
2025-02-17 23:21:27 +0530

    User input symptoms

--- covid/c.py
+++ covid/c.py
@@ -1,2 +1,5 @@
 # List of common COVID symptoms
 covid_symptoms = ["fever", "cough", "tiredness", "loss of taste", "difficulty breathing"]
+
+# User input symptoms
+user_symptoms = input("Enter your symptoms (comma separated): ").lower().split(",")
