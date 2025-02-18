92a79877611a3b21b655da8a960d5f6c53dfef40
RaviKumar <ravikumarainapudi97@gmail.com>
2025-02-18 22:23:11 +0530

    try

--- Weekday Checker/w.py
+++ Weekday Checker/w.py
@@ -7,3 +7,6 @@ date_input = input("Enter a date (YYYY-MM-DD): ")
 # Convert to datetime object
 
 try:
+   date_obj = datetime.datetime.strptime(date_input, "%Y-%m-%d")
+    weekday = date_obj.strftime("%A")  # Get the day of the week
+    print(f"The day of the week for {date_input} is {weekday}.") 
